200cf19f3d8e13f49cce40f7a9ec53c237d27ec5
Caitriona71 <caitrionaboyle@ymail.com>
2022-08-30 15:16:05 +0000

    Add a variable for circumstances that make a win

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 const squares = document.querySelectorAll(".square");
 const statusOfPlay = document.querySelector("#statusOfPlay");
+const newGameBtn = document.querySelector("#newGameBtn");
+const winCircumstances = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
